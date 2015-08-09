026edb044b014f122168693dc07896ac8b26dacd
mmx900 <mmx900@gmail.com>
2015-08-09 15:53:49 +0900

    코드 정리

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,6 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'jett.ionic.filter.bar', 'starter.controllers'])
 
-	.run(function ($ionicPlatform, $cordovaSplashscreen, $cordovaToast, $rootScope) {
+	.run(function ($ionicPlatform, $cordovaSplashscreen, $cordovaToast) {
 		$ionicPlatform.ready(function () {
 			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 			// for form inputs)
@@ -14,14 +8,15 @@ angular.module('starter', ['ionic', 'ngCordova', 'jett.ionic.filter.bar', 'start
 				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 				cordova.plugins.Keyboard.disableScroll(true);
 
-				$cordovaToast.showLongBottom('버전을 확인하고 있습니다.').then(function(success) {
-					setTimeout(function() {
-						$cordovaSplashscreen.hide();
-					}, 5000);
-					// success
-				}, function (error) {
-					// error
-				});
+				$cordovaToast.showLongBottom('버전을 확인하고 있습니다.')
+					.then(function (success) {
+						setTimeout(function () {
+							$cordovaSplashscreen.hide();
+						}, 5000);
+						// success
+					}, function (error) {
+						// error
+					});
 
 			}
 			if (window.StatusBar) {
