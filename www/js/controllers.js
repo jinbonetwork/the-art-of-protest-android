ab8317b6aa9ce9ff4550dd34b79780b1e7138744
mmx900 <mmx900@gmail.com>
2015-09-14 20:39:57 +0900

    미사용 로그인 코드 제거

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,6 @@ angular.module('starter.controllers', ['starter.services'])
 
 	.controller('AppCtrl', function ($scope, $ionicModal, $timeout, $http, $localStorage, categories,
 									 $ionicSideMenuDelegate, $log) {
-		//TODO localstorage 이용 버전 확인
 		$scope.categories = categories;
 
 		/*
@@ -27,37 +26,6 @@ angular.module('starter.controllers', ['starter.services'])
 		// listen for the $ionicView.enter event:
 		//$scope.$on('$ionicView.enter', function(e) {
 		//});
-
-		// Form data for the login modal
-		$scope.loginData = {};
-
-		// Create the login modal that we will use later
-		$ionicModal.fromTemplateUrl('templates/login.html', {
-			scope: $scope
-		}).then(function (modal) {
-			$scope.modal = modal;
-		});
-
-		// Triggered in the login modal to close it
-		$scope.closeLogin = function () {
-			$scope.modal.hide();
-		};
-
-		// Open the login modal
-		$scope.login = function () {
-			$scope.modal.show();
-		};
-
-		// Perform the login action when the user submits the login form
-		$scope.doLogin = function () {
-			$log.log('Doing login', $scope.loginData);
-
-			// Simulate a login delay. Remove this and replace with your login
-			// code if using a login system
-			$timeout(function () {
-				$scope.closeLogin();
-			}, 1000);
-		};
 
 		$scope.toggleLeftSideMenu = function () {
 			$ionicSideMenuDelegate.toggleLeft();
